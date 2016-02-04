d002b001fcf222701b9f95c8cc9d3ebd49718ede
kumaresh cj <cjkumaresh@gmail.com>
2016-02-05 01:39:36 +0530

    added router changes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 exports.startServer = function (http, fs) {
-    var server = http.createServer(function (request, response) {
+    var Router = require('node-simple-router');
+    var router = new Router();
+    
+    router.get("/remote", function (request, response) {
+        response.end("remote");
+    });
+    
+    router.get("/", function (request, response) {
         var fsPath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
         fs.readdir(fsPath,function (err, files) {
             response.write(fsPath);
@@ -11,6 +18,8 @@ exports.startServer = function (http, fs) {
         });
 
     });
+    
+    var server = http.createServer(router);
     server.listen(8881,function () {
        console.log('server is listening'); 
     });
