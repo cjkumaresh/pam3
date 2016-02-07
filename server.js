a4cffefd73a3b0c4de6186a76ae22d00277c4b9b
kumaresh cj <cjkumaresh@gmail.com>
2016-02-08 02:19:28 +0530

    cleaned up little bit

--- server.js
+++ server.js
@@ -3,30 +3,13 @@ const config = require('./config.json'),
     Router = require('node-simple-router'),
     router = new Router(),
     fsPath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
-    finalHandler = require('finalhandler'),
     http = require('http'),
     serveStatic = require('serve-static'),
-    serve = serveStatic('public', {'index': false});
+    serve = serveStatic('public', {'index': 'index.html'});
     
 let server;
 
 exports.startServer = function (http, fs) {    
-    //initialize router 
-    router.get("/remote", function (req, res) {
-        res.end("remote");
-    });
-    
-    router.get("/getFiles", function (req, res) {
-        fs.readdir(fsPath,function (err, files) {
-            res.write(fsPath);
-            for (let key in files) {
-                res.write('\n');
-                res.write(files[key]);
-            }
-            res.end();
-        });
-    });
-    
     //initialize server
     server = http.createServer(router);
     server.listen(config.port,function () {
