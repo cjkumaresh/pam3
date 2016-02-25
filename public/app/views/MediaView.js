78c3cd70bfc74adf9bc4ddfa1b53b0fbc282e2d1
kumaresh cj <cjkumaresh@gmail.com>
2016-02-25 10:47:20 +0530

    added changes for using express js framework

--- public/app/views/MediaView.js
+++ public/app/views/MediaView.js
@@ -7,23 +7,13 @@ define([
     return Backbone.View.extend({
         el: '#media-view',
         
-        initialize: function () {
-            this.model.fileName = this.getFileName(location.href);
-            this.model.type = 'picture';  
-            this.model.src = this.model.src;    
+        initialize: function () {    
             this.render();
         },
         
         render: function () {
             $('#file-system-view').html('');
-            let mediaFileViewTemplate = _.template($("#media-file-view-template").html());
             this.$el.append(mediaFileViewTemplate(this.model));
-        },
-        
-        getFileName: function (url) {
-            var parseUrl = url.split('/');
-            var fileName = parseUrl[parseUrl.length-1];
-            return fileName;
         }
     });
 });
--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 'use strict';
 const config = require('./config.json'),
+    express = require('express'),
     Router = require('node-simple-router'),
     router = new Router(),    
     http = require('http'),
-    filter = require('./lib/utils/filter');
-    //serveStatic = require('serve-static')
+    filter = require('./lib/utils/filter'),
+    bodyParser = require('body-parser'),
+    multer = require('multer');
     
     
-let server;
+    
+let server,
+    app = express(),
+    upload = multer();
+    
 const fsPath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
 exports.startServer = function (http, fs) {    
-    //initialize server    
-    router.get('/getFiles', function (req, res) {
-        try {
-            fs.accessSync(fsPath, fs.F_OK);
-            fs.readdir(fsPath, function (err, files) {
-                res.setHeader('Content-Type', 'application/json');
-                //files = files.filter(filter.getProperFiles);
-                res.end(JSON.stringify({'path':fsPath,'files':files}));
-            });
-        } catch (e) {
-            throw e;
-        }
+    //initialize server
+    app.use(express.static('public'));
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+    // app.get('/', function (req, res) {
+    //     sendFileList(req, res, fs);        
+    // });
         
+    app.get('/getFiles', function (req, res) {
+        sendFileList(req, res, fs);        
     });
     
-     router.post('/navigate', function (req, res) {
+     app.post('/navigate', function (req, res) {
+        if (!req.body) 
+            return res.sendStatus(400);
         var path = fsPath;
-        var accessPath = req.post.path.split('/');
+        var accessPath = req.body.path.split('/');
         for (var k in accessPath) {
             path = path + '\\' + accessPath[k];
         }
@@ -38,29 +43,37 @@ exports.startServer = function (http, fs) {
                 fs.readdir(path, function (err, files) {
                     res.setHeader('Content-Type', 'application/json');
                     //files = files.filter(filter.getProperFiles);
-                    res.end(JSON.stringify({'path':path,'files':files, 'params':req.post.path}));
+                    res.end(JSON.stringify({'path':path,'files':files, 'params':req.body}));
                 });    
             } else {
-                var readableStream = fs.createReadStream(path);
-                readableStream.setEncoding('utf8');
-                readableStream.pipe(res);
+                // var readableStream = fs.createReadStream(path);
+                // readableStream.setEncoding('utf8');
+                // readableStream.pipe(res);
+                // var file = fs.readFileSync(path);
+                 res.writeHead(200, {'Content-Type': 'image/jpg' });
+                // res.end(bin2string(file));
+                res.sendFile(path);
             }
             
          } catch (e) {
             throw e;
         }
     });
-    
-    router.post('/openFile', function (req, res) {
-        var path = fsPath +'\\' + decodeURIComponent(req.post.fileName);
-        var readableStream = fs.createReadStream(path);
-        readableStream.setEncoding('utf8');
-        res.setHeader('content-type', 'text/plain');
-        readableStream.pipe(res);
-    });
-    
-    server = http.createServer(router);
-    server.listen(config.port, function () {
-       console.log('server is listening'); 
-    });
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!');
+    });    
 };
+
+function sendFileList(req, res, fs) {
+    try {
+        fs.accessSync(fsPath, fs.F_OK);
+        fs.readdir(fsPath, function (err, files) {
+            res.setHeader('Content-Type', 'application/json');
+            //files = files.filter(filter.getProperFiles);
+            res.end(JSON.stringify({'path':fsPath,'files':files}));
+        });
+    } catch (e) {
+            throw e;
+    }
+}
