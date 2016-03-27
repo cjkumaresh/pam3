b03c167d35a6a9151d19f20b3f5e0715b0336e55
kumaresh cj <cjkumaresh@gmail.com>
2016-03-27 23:08:57 +0530

    code cleanup

--- public/app/views/VideoView.js
+++ public/app/views/VideoView.js
@@ -1,21 +1,21 @@
 'use strict';
 define([
-    'backbone',      
+    'backbone'
 ], function (Backbone) {
-     return Backbone.View.extend({
-         el: '#view',
-         
-         initialize: function () {
-             this.$el.find('#file-system-view').html('');
-             this.$el.find('#media-view').html('');
-             this.render();
-         },
-         
-         render: function () {
-             var url = '/video?path=' + this.model.replace(/\//g, 'pam3'),
-                 $video = '<video class="col-xs-12 media" src ="' + url + '" type="video/mp4" controls/>';
-             this.$el.html($video);
-         }
-         
-     });   
+    return Backbone.View.extend({
+        el: '#view',
+
+        initialize: function () {
+            this.$el.find('#file-system-view').html('');
+            this.$el.find('#media-view').html('');
+            this.render();
+        },
+
+        render: function () {
+            var url = '/video?path=' + this.model.replace(/\//g, 'pam3'),
+                $video = '<video class="col-xs-12 media" src ="' + url + '" type="video/mp4" controls/>';
+            this.$el.html($video);
+        }
+
+    });
 });
