4fc83f9e88d185cd33e7b30499b656d2d836eb9e
kumaresh cj <cjkumaresh@gmail.com>
2016-05-17 23:54:51 +0530

    moved swipe implementation to new util

---
+++ public/app/utils/swipe.js
@@ -0,0 +1,29 @@
+define([
+    'jquery'
+], function($) {
+    var swipe = function(swipeDirection) {
+        var currentFile = "";
+        var url = location.href.split("/");
+        var index = 0;
+        var newFile = "";
+        currentFile = url[url.length - 1];
+        this.files = JSON.parse(sessionStorage.getItem("pam-files"));
+        index = this.files.indexOf(currentFile);
+
+        if (index !== -1 && currentFile.split('.')[1]) {
+            if (swipeDirection === "prev" && index !== 0) {
+                index--;
+                newFile = this.files[index];
+            } else if (swipeDirection === "next") {
+                index++;
+                newFile = this.files[index];
+            }
+        }
+
+        if (newFile) {
+            url[url.length - 1] = newFile;
+            location.href = url.join("/");
+        }
+    }
+    return swipe;
+});
--- public/app/views/AppView.js
+++ public/app/views/AppView.js
@@ -1,8 +1,9 @@
 'use strict';
 define([
     'jquery',
-    'backbone'
-], function ($, Backbone) {    
+    'backbone',
+    'utils/swipe'
+], function ($, Backbone, swipe) {    
     return Backbone.View.extend({
         el: 'body',
 
@@ -20,29 +21,8 @@ define([
         },
         
         handleNavigate: function (e) {
-            var currentFile = "";
-            var url = location.href.split("/");
-            var index = 0;
-            var newFile = "";
-            currentFile = url[url.length - 1];
-            this.files = JSON.parse(sessionStorage.getItem("pam-files"));
-            index = this.files.indexOf(currentFile);
-            
-            if (index !== -1 && currentFile.split('.')[1]) {
-                if ($(e.target).text() === "prev" && index !== 0) {
-                    index--;
-                    newFile = this.files[index];
-                } else if ($(e.target).text() === "next") {
-                    index++;
-                    newFile = this.files[index];
-                }
-            }
-            
-            if (newFile) {
-                url[url.length - 1] = newFile;
-                location.href = url.join("/");
-            }
-             
+            var swipeDirection = $(e.target).text();
+            swipe(swipeDirection); 
         }
     });    
 }); 
