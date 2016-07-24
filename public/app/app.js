a58be9364ee84e66d12c378ad3c10b11eec56914
kumaresh cj <cjkumaresh@gmail.com>
2016-07-24 23:55:53 +0530

    Fixed dropdown menu issue

--- public/app/app.js
+++ public/app/app.js
@@ -4,10 +4,14 @@ requirejs.config({
         jquery: 'lib/jquery',
         underscore: 'lib/underscore',
         backbone: 'lib/backbone',
+        bootstrap: 'lib/bootstrap.min',
         jquerySwipe: 'lib/jquery.touchSwipe.min'
     },
     shim: {
         jquerySwipe: {
+            deps: ['jquery']
+        },
+        bootstrap: {
             deps: ['jquery']
         }
     }
